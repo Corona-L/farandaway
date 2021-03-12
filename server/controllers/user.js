e979dac55c30f64503deabf843156ee038ed773c
Corona Laufer <laufer.corona@hotmail.com>
2021-03-12 19:30:29 +0100

    refactor: small changes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,6 @@
 'use-strict';
 
-const {models} = require('../models');
+const { models } = require('../models');
 const { user, trip, option, invitee } = models;
 
 exports.createUser = async (req, res) => {
