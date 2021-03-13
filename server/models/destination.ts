feed94ed1054d4bd39d0a22b4614bb43651cd44c
Corona Laufer <laufer.corona@hotmail.com>
2021-03-13 12:06:55 +0100

    merge conflicts

--- server/models/destination.ts
+++ server/models/destination.ts
@@ -1,24 +1,5 @@
 import { Sequelize, DataTypes, Optional, Model } from 'sequelize';
-<<<<<<< HEAD
 import { DestinationInstance } from './interfaces';
-=======
-
-interface DestinationAttributes {
-  id?: number;
-  name: string;
-  country: string;
-  emoji: string;
-  timezone: string;
-  climate: object[]; // need to define interface for climate json
-}
-
-interface DestinationCreationAttributes
-  extends Optional<DestinationAttributes, 'id'> {}
-
-interface DestinationInstance
-  extends Model<DestinationAttributes, DestinationCreationAttributes>,
-    DestinationAttributes {}
->>>>>>> development
 
 module.exports = (sequelize:Sequelize) => {
   sequelize.define<DestinationInstance>('destination', {
