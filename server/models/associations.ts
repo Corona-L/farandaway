37c7b376a1d9db248df669b5792807534c9a4872
Ben Pearce <benpearce9@live.co.uk>
2021-03-13 10:45:45 +0000

    convert associations to ts loosely

---
+++ server/models/associations.ts
@@ -0,0 +1,31 @@
+import { Sequelize } from 'sequelize';
+
+function associations (sequelize: Sequelize) {
+	const { trip, user, option, invitee } = sequelize.models;
+  
+  // User-Trip associations
+  user.hasMany(trip);
+  trip.belongsTo(user, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+
+  // Trip-Option associations
+  trip.hasMany(option, { onDelete: 'cascade', hooks: true });
+  option.belongsTo(trip, {
+    foreignKey: {
+      allowNull: false, //this will be tripId
+    },
+  });
+
+  // Trip-invitee associations
+  trip.hasMany(invitee);
+  invitee.belongsTo(trip, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+}
+
+module.exports = associations;
