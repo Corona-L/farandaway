0d3b6e24b18029f32dabcb46f2874d3cb5b492fc
Corona Laufer <laufer.corona@hotmail.com>
2021-03-15 20:42:26 +0100

    test: user dashboard no trips

--- client/src/app/user-dashboard/user-dashboard-notrips/user-dashboard-notrips.component.spec.ts
+++ client/src/app/user-dashboard/user-dashboard-notrips/user-dashboard-notrips.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDashboardNotripsComponent } from './user-dashboard-notrips.component';
+import { By } from '@angular/platform-browser';
+
+
 
 describe('UserDashboardNotripsComponent', () => {
   let component: UserDashboardNotripsComponent;
@@ -17,6 +20,12 @@ describe('UserDashboardNotripsComponent', () => {
     fixture = TestBed.createComponent(UserDashboardNotripsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should have a no trips message', () => {
+    const message = fixture.debugElement.query(By.css('h3'));
+    expect(message.nativeElement.innerText).toContain('No trips to display');
+
   });
 
   it('should create', () => {
