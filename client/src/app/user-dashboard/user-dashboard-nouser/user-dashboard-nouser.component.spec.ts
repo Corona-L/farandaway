71007256b2e459b643f49ee6874db7ed7db96877
Corona Laufer <laufer.corona@hotmail.com>
2021-03-15 20:36:01 +0100

    test: user dashboard no user

--- client/src/app/user-dashboard/user-dashboard-nouser/user-dashboard-nouser.component.spec.ts
+++ client/src/app/user-dashboard/user-dashboard-nouser/user-dashboard-nouser.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserDashboardNouserComponent } from './user-dashboard-nouser.component';
+import { By } from '@angular/platform-browser';
 
 describe('UserDashboardNouserComponent', () => {
   let component: UserDashboardNouserComponent;
@@ -18,6 +18,20 @@ describe('UserDashboardNouserComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+
+  it('should include a h1 header', () => {
+    const h1 = fixture.debugElement.query(By.css('h1'));
+    expect(h1).toBeTruthy();
+    expect(h1.nativeElement.innerText).toContain('Log in to see all');
+  });
+
+  it('should have a login button', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button).toBeTruthy();
+    expect(button.nativeElement.innerText).toEqual('Log in');
+  });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
