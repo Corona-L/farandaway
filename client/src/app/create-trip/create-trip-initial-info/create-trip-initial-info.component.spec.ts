1be9e9820a98b35014e2fac317788b206dd67765
Ben Pearce <benpearce9@live.co.uk>
2021-03-15 19:25:26 +0000

    test: create-trip-initial-info.ts rendering checked

--- client/src/app/create-trip/create-trip-initial-info/create-trip-initial-info.component.spec.ts
+++ client/src/app/create-trip/create-trip-initial-info/create-trip-initial-info.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateTripInitialInfoComponent } from './create-trip-initial-info.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CreateTripInitialInfoComponent', () => {
   let component: CreateTripInitialInfoComponent;
   let fixture: ComponentFixture<CreateTripInitialInfoComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +19,41 @@ describe('CreateTripInitialInfoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateTripInitialInfoComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Render h2 correctly
+  it('should render h2 correctly', () => {
+    const packYourBags = de.query(By.css('h2'));
+    expect(packYourBags.nativeElement.innerText).toContain('Pack your bags!');
+  });
+
+  // Render p correctly
+  it('should render p tag correctly', () => {
+    const p = de.query(By.css('p'));
+    expect(p.nativeElement.innerText).toContain('First thing\'s first');
+  });
+
+  // Render form and inputs 
+  it('should render add trip info form', () => {
+    const form = de.query(By.css('form'));
+    expect(form).toBeTruthy();
+
+    const titleInput = de.query(By.css('input[name=title]'));
+    const originInput = de.query(By.css('input[name=origin]'));
+    expect(titleInput).toBeTruthy();
+    expect(originInput).toBeTruthy();
+  });
+
+  // Render get started button
+  it('should render get started button', () => {
+    const button = de.query(By.css('button'));
+    expect(button.nativeElement.innerText).toBe('Get started');
+  });
+
 });
