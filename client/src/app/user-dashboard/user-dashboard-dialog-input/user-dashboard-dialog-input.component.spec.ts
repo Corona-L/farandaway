9bb4e7016e1f2845a208d4f0a2f2c7c6284c103d
Corona Laufer <laufer.corona@hotmail.com>
2021-03-16 10:30:30 +0100

    test: user dashboard

--- client/src/app/user-dashboard/user-dashboard-dialog-input/user-dashboard-dialog-input.component.spec.ts
+++ client/src/app/user-dashboard/user-dashboard-dialog-input/user-dashboard-dialog-input.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { UserDashboardDialogInputComponent } from './user-dashboard-dialog-input.component';
 
@@ -18,6 +19,12 @@ describe('UserDashboardDialogInputComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  // FIX: NullInjectorError: R3InjectorError(DynamicTestModule)[MatDialogRef -> MatDialogRef]:
+  // it('should include h1 header', () => {
+  //   const h1 = fixture.debugElement.query(By.css('h1'));
+  //   expect(h1.nativeElement.innerText).toBe('Trips are better');
+  // });
 
   // it('should create', () => {
   //   expect(component).toBeTruthy();
