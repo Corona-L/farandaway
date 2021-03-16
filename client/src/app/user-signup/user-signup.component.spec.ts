2564dbc2b74fa52af07c36f9e0a40bddc6697680
Corona Laufer <laufer.corona@hotmail.com>
2021-03-16 18:27:10 +0100

    test: api call upon user signup

--- client/src/app/user-signup/user-signup.component.spec.ts
+++ client/src/app/user-signup/user-signup.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserSignupComponent } from './user-signup.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiClientService } from '../api-client.service';
+import { DebugElement } from '@angular/core';
+
+const apiClientServiceStub = {
+  userSignUp: () => {
+    return {
+      subscribe: () => 'user signed up'
+    }
+  }
+};
 
 describe('UserSignupComponent', () => {
   let component: UserSignupComponent;
   let fixture: ComponentFixture<UserSignupComponent>;
+  let de: DebugElement;
+  let apiClientService: ApiClientService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserSignupComponent ]
+      declarations: [ UserSignupComponent ],
+      imports: [ RouterTestingModule, HttpClientModule ],
+      providers: [{ provide: ApiClientService, useValue: apiClientServiceStub}]
+
     })
     .compileComponents();
   });
@@ -16,10 +32,19 @@ describe('UserSignupComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserSignupComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    apiClientService = de.injector.get(ApiClientService);
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('userSignup should call API client service', () => {
+    spyOn(apiClientService, 'userSignUp').and.callThrough();
+    component.userSignUp();
+    fixture.detectChanges();
+    expect(apiClientService.userSignUp).toHaveBeenCalled();
+  });
 });
