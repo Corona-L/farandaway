d8bf0e518225d170aecd503893db42666716a739
Ben Pearce <benpearce9@live.co.uk>
2021-03-16 17:29:03 +0000

    test: trip-poll component rendering and api call

--- client/src/app/trip-poll/trip-poll.component.spec.ts
+++ client/src/app/trip-poll/trip-poll.component.spec.ts
@@ -1,14 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiClientService } from '../api-client.service';
 
 import { TripPollComponent } from './trip-poll.component';
+
+const apiClientServiceStub = {
+  getTrip: () => {
+    return {
+      subscribe: () => {}
+    }
+  }
+};
 
 describe('TripPollComponent', () => {
   let component: TripPollComponent;
   let fixture: ComponentFixture<TripPollComponent>;
+  let de: DebugElement;
+  let apiClientService: ApiClientService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TripPollComponent ]
+      declarations: [ TripPollComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [{provide: ApiClientService, useValue: apiClientServiceStub}]
     })
     .compileComponents();
   });
@@ -16,10 +32,44 @@ describe('TripPollComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TripPollComponent);
     component = fixture.componentInstance;
+    component.trip = {
+      id: 1,
+      title: 'Summer holiday',
+      options: [{
+        title: 'string',
+        destination: 'string',
+        startDate: '2011-10-10T14:48:00',
+        nights: 1,
+        budgetRangeMin: 1,
+        budgetRangeMax: 1,
+        votes: 1,
+        isChosen: true,
+      }]
+    };
+    de = fixture.debugElement;
+    apiClientService = de.injector.get(ApiClientService);
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should properly render elements', () => {
+    const h1 = de.nativeElement.querySelector('h1');
+    const p = de.nativeElement.querySelector('p');
+    const votingContainer = de.nativeElement.querySelector('.voting_container');
+    if (!component.trip) throw new Error('No trip has been supplied');
+    expect(h1.innerText).toBe(component.trip.title);
+    expect(p).toBeTruthy();
+    expect(votingContainer).toBeTruthy();
+  });
+
+  it('should call get api service when getTrip is called', () => {
+    spyOn(apiClientService, 'getTrip').and.callThrough();
+    if (!component || !component.trip || !component.trip.id) throw new Error('!!!! No trip has been supplied');
+    component.getTrip(component.trip.id);
+    expect(apiClientService.getTrip).toHaveBeenCalledWith(component.trip.id);
+  });
+
 });
