540a4aa7a2f382054ee5bd9268efbdce8a6ed6c1
Corona Laufer <laufer.corona@hotmail.com>
2021-03-17 10:44:35 +0100

    test: header burger logout func

--- client/src/app/header/header-burger/header-burger.component.spec.ts
+++ client/src/app/header/header-burger/header-burger.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { By } from '@angular/platform-browser';
 
 import { HeaderBurgerComponent } from './header-burger.component';
+
+import { UserInfoService } from '../../user-info.service';
+
+const userInfoServiceStub = {
+  changeCurrentUserId: () => {}
+}
 
 describe('HeaderBurgerComponent', () => {
   let component: HeaderBurgerComponent;
   let fixture: ComponentFixture<HeaderBurgerComponent>;
+  let de: DebugElement;
+  let userInfoService: UserInfoService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderBurgerComponent ]
+      declarations: [ HeaderBurgerComponent ],
+      imports: [ RouterTestingModule, HttpClientModule, MatMenuModule ],
+      providers: [{ provide: UserInfoService, useValue: userInfoServiceStub }],
     })
     .compileComponents();
   });
@@ -16,10 +31,20 @@ describe('HeaderBurgerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderBurgerComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    userInfoService = de.injector.get(UserInfoService);
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call userInfoService.changeCurrentUserId when logOut() is called', () => {
+    spyOn(userInfoService, 'changeCurrentUserId');
+    component.logOut();
+    fixture.detectChanges();
+    expect(userInfoService.changeCurrentUserId).toHaveBeenCalled();
+  });
+
 });
