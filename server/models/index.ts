2a42743c25250a96bde39fed34f8cdad162920b7
Ben Pearce <benpearce9@live.co.uk>
2021-03-20 15:47:33 +0000

    commit

--- server/models/index.ts
+++ server/models/index.ts
@@ -7,15 +7,6 @@ import associations from './associations';
 if (typeof config.DATABASE_URL !== 'string') throw new Error('database url missing');
 const sequelize = new Sequelize(config.DATABASE_URL);
 
-
-  // config.DB_NAME,
-  // config.DB_USERNAME,
-  // config.DB_PASSWORD,
-  // {
-  //   host: config.DB_HOST,
-  //   dialect: config.DB_DIALECT,
-  // },
-
 const modelDefiners = [
   require('./user'),
   require('./trip'),
